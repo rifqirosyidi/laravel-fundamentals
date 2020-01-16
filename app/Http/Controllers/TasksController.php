76a1f0cd1a020c2e1617c6d31c04d6eb055b0b75
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-01-16 22:27:45 +0700

    controller

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TasksController extends Controller
+{
+    //
+    public function index() {
+    	
+    	$tasks = Task::all();
+
+    	// return view('welcome', ['tasks' => $tasks ]);
+    	return view('tasks.index', compact('tasks'));
+    }
+
+    public function show($id) {
+
+    	$task = Task::find($id);
+	
+		return view('tasks.show', compact('task'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,6 @@
 |
 */
 
-Route::get('/tasks', function () {
-
-	$tasks = App\Task::all();
-
-    // return view('welcome', ['tasks' => $tasks ]);
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-	
-	$task = App\Task::find($id);
-	
-	return view('tasks.show', compact('task'));
-
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
